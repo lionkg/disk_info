d84d61ec7a51fd4087314b665f13abca925f2c68
Лев Тен <l.ten@reg.ru>
2020-02-16 22:17:23 +0600

    Unified uniq_id for array class. Started creating the class for Adaptec arrays.

--- raidarray.py
+++ raidarray.py
@@ -9,13 +9,13 @@ class RaidArray(object):
     An abstract class for RAID arrays.
     """
     def __init__(self, controller_id, array_id):
-        self.level = self.status = self.scsi_id = self.pd_list = \
+        self.level = self.status = self.unique_id = self.pd_list = \
             self.access = self.cache = self.size = None
         self.controller_id = controller_id
         self.id = array_id
         self.pd_list = []
         self.get_attrs()
-        if self.scsi_id is None:
+        if self.unique_id is None:
             pass
         self.get_blk_dev_name()
         self.get_pd_list()
@@ -66,11 +66,11 @@ class RaidArrayMegaRaid(RaidArray):
                     self.cache = line.split()[3]
                     self.size = line.split()[2]
                 elif line.lower().startswith('scsi naa id'):
-                    self.scsi_id = line.split()[-1].lower()
+                    self.unique_id = line.split()[-1].lower()
 
     def get_blk_dev_name(self):
         get_blk_dev_name_cmd = \
-            'readlink -f /dev/disk/by-id/wwn-0x'+self.scsi_id
+            'readlink -f /dev/disk/by-id/wwn-0x'+self.unique_id
         try:
             self.blk_dev_name = subprocess.check_output(
                 get_blk_dev_name_cmd, shell=True,
@@ -85,3 +85,38 @@ class RaidArrayMegaRaid(RaidArray):
         self.pd_list = tuple(self.pd_list)
         for pd in self.pd_list:
             pd.append(BlkDev(self.blk_dev_name, 'megaraid', pd[1]))
+
+
+class RaidArrayAdaptec(RaidArray):
+
+    # get array info
+    def get_info(self):
+        array_get_info_command = \
+            'arcconf getconfig 1 LD '+str(self.id)
+        try:
+            self.info = subprocess.check_output(
+                array_get_info_command, shell=True,
+                universal_newlines=True).splitlines()
+        except subprocess.CalledProcessError:
+            self.info = None
+
+    def get_attrs(self):
+        self.get_info()
+        if self.info is not None:
+            for line in self.info:
+                if line.strip().startswith('RAID level'):
+                    self.level = line.split()[-1]
+                elif line.strip().startswith('Status of Logical Device'):
+                    self.status = line.split()[-1]
+                elif line.strip().startswith('Write-cache status'):
+                    self.cache = line.split()[-1]
+                elif line.strip().startswith('Size'):
+                    self.size = line.split()[-1]
+                elif line.strip().startswith('Unique Identifier'):
+                    self.unique_id = line.split()[-1]
+
+    def get_blk_dev_name(self):
+        return None
+
+    def get_pd_list(self):
+        return None
