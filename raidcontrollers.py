43e9783894f60aa678b55be641f9005c7ca77c5a
Лев Тен <l.ten@reg.ru>
2020-02-26 20:20:05 +0600

    Refactored raid_controllers attribute initialization.

--- raidcontrollers.py
+++ raidcontrollers.py
@@ -10,8 +10,7 @@ class RaidControllers(object):
     """
 
     def __init__(self):
-        self.raid_controllers = {}
-        self.get_controllers()
+        self.raid_controllers = self.get_controllers()
 
     # abstract method, do not use directly
     def get_controllers(self):
@@ -22,15 +21,27 @@ class RaidControllers(object):
     def get_controller_arrays(self):
         raise NotImplementedError
 
-    # abstract method, do not use directly
-    # get array by some attribute
+    # get array object by some attribute
     def fetch_array(self, array_attr_name, array_attr_value):
-        raise NotImplementedError
+        for controller_id in self.raid_controllers:
+            for array in self.raid_controllers[controller_id]:
+                try:
+                    if getattr(array[2], array_attr_name) == \
+                            array_attr_value:
+                        return controller_id, array[0], array[2]
+                except AttributeError:
+                    return None
 
-    # abstract method, do not use directly
-    # get array's physical devices list by some attribute
+    # get array's physical devices list
     def fetch_array_pd_list(self, array_attr_name, array_attr_value):
-        raise NotImplementedError
+        for controller_id in self.raid_controllers:
+            for array in self.raid_controllers[controller_id]:
+                try:
+                    if getattr(array[2], array_attr_name) == \
+                            array_attr_value:
+                        return array[2].pd_list
+                except AttributeError:
+                    return None
 
 
 class RaidControllersMegaRaid(RaidControllers):
@@ -52,22 +63,22 @@ class RaidControllersMegaRaid(RaidControllers):
 
     # get controllers list
     def get_controllers(self):
-        self.controller_id_pattern = 'Controller ='
-        self.controller_get_info_command = 'storcli64 /call show'
+        raid_controllers = {}
+        controller_id_pattern = 'Controller ='
+        controller_get_info_command = 'storcli64 /call show'
 
         try:
-            self.raid_controller_info = subprocess.check_output(
-                self.controller_get_info_command, shell=True,
+            raid_controller_info = subprocess.check_output(
+                controller_get_info_command, shell=True,
                 universal_newlines=True).splitlines()
         except subprocess.CalledProcessError:
-            self.raid_controllers_list = None
             return None
 
-        for line in self.raid_controller_info:
-            if line.strip().startswith(self.controller_id_pattern):
-                if int(line.split()[-1]) not in self.raid_controllers:
-                    self.raid_controllers[int(line.split()[-1])] = []
-        return None
+        for line in raid_controller_info:
+            if line.strip().startswith(controller_id_pattern):
+                if int(line.split()[-1]) not in raid_controllers:
+                    raid_controllers[int(line.split()[-1])] = []
+        return raid_controllers
 
     # get raid arrays for raid controller
     def get_controller_arrays(self, raid_controller_id):
@@ -82,28 +93,6 @@ class RaidControllersMegaRaid(RaidControllers):
                 arrays_list.append(
                     int(line.split('/')[1].split()[0]))
         return arrays_list
-
-    # get array coordinates by attribute
-    def fetch_array(self, array_attr_name, array_attr_value):
-        for controller_id in self.raid_controllers:
-            for array in self.raid_controllers[controller_id]:
-                try:
-                    if getattr(array[2], array_attr_name) == \
-                            array_attr_value:
-                        return controller_id, array[0], array[2]
-                except AttributeError:
-                    return None
-
-    # get array's physical devices list
-    def fetch_array_pd_list(self, array_attr_name, array_attr_value):
-        for controller_id in self.raid_controllers:
-            for array in self.raid_controllers[controller_id]:
-                try:
-                    if getattr(array[2], array_attr_name) == \
-                            array_attr_value:
-                        return array[2].pd_list
-                except AttributeError:
-                    return None
 
 
 class RaidControllersAdaptec(RaidControllers):
@@ -125,25 +114,25 @@ class RaidControllersAdaptec(RaidControllers):
 
     # get controllers list
     def get_controllers(self):
+        raid_controllers = {}
         controller_id_pattern = 'Controller [0-9]+:'
         controller_get_info_command = 'arcconf list'
 
         try:
-            self.raid_controller_info = subprocess.check_output(
+            raid_controller_info = subprocess.check_output(
                 controller_get_info_command, shell=True,
                 universal_newlines=True).splitlines()
         except subprocess.CalledProcessError:
-            self.raid_controllers_list = None
             return None
 
-        for line in self.raid_controller_info:
+        for line in raid_controller_info:
             if re.match(controller_id_pattern, line.strip()):
                 if int(line.split()[1].rstrip(':')) not in \
-                                        self.raid_controllers:
-                    self.raid_controllers[
+                                        raid_controllers:
+                    raid_controllers[
                         int(line.split()[1].rstrip(':'))
                         ] = []
-        return None
+        return raid_controllers
 
     # get raid arrays for raid controller
     def get_controller_arrays(self, raid_controller_id):
@@ -158,25 +147,3 @@ class RaidControllersAdaptec(RaidControllers):
                 arrays_list.append(
                     int(line.split()[-1]))
         return arrays_list
-
-    # get array coordinates by attribute
-    def fetch_array(self, array_attr_name, array_attr_value):
-        for controller_id in self.raid_controllers:
-            for array in self.raid_controllers[controller_id]:
-                try:
-                    if getattr(array[2], array_attr_name) == \
-                            array_attr_value:
-                        return controller_id, array[0], array[2]
-                except AttributeError:
-                    return None
-
-    # get array's physical devices list
-    def fetch_array_pd_list(self, array_attr_name, array_attr_value):
-        for controller_id in self.raid_controllers:
-            for array in self.raid_controllers[controller_id]:
-                try:
-                    if getattr(array[2], array_attr_name) == \
-                            array_attr_value:
-                        return array[2].pd_list
-                except AttributeError:
-                    return None
