42b33a3421bc7974f915a83b18991cc8cdc7e110
Лев Тен <l.ten@reg.ru>
2020-02-26 20:06:13 +0600

    Added if __name__ == '__main__'.

--- disk.py
+++ disk.py
@@ -12,46 +12,54 @@ def check4tool(name):
     if distutils.spawn.find_executable(name) is None:
         print('CRITICAL: {} could not be found'.format(name))
         exit(1)
+    return True
 
 
-# Check for lspci
-check4tool('lspci')
+if __name__ == '__main__':
+    # Check for lspci
+    check4tool('lspci')
 
-# Check if the server is a VM
-is_vm = subprocess.check_output("lspci", shell=True,
-                                universal_newlines=True).splitlines()
-for line in is_vm:
-    if "virtio" in line.lower():
-        print("OK: this is a virtual machine")
-        exit(0)
+    # Check if the server is a VM
+    is_vm = subprocess.check_output("lspci", shell=True,
+                                    universal_newlines=True).splitlines()
+    for line in is_vm:
+        if "virtio" in line.lower():
+            print("OK: this is a virtual machine")
+            exit(0)
 
-# Check for the other tools
-required_tools = ('lsblk', 'smartctl', 'parted')
-for tool in required_tools:
-    check4tool(tool)
+    # Check for the other tools
+    required_tools = ('lsblk', 'smartctl', 'parted')
+    for tool in required_tools:
+        check4tool(tool)
 
-# Get the full list of physical dirves with parameters
-try:
-    blkdev_list = \
-        subprocess.check_output('lsblk -dn -o NAME', shell=True,
-                                universal_newlines=True).splitlines()
-except subprocess.CalledProcessError:
-    print("CRITICAL: could not get list of physical drives")
-    exit(2)
+    # Get the full list of physical dirves with parameters
+    try:
+        blkdev_list = \
+            subprocess.check_output('lsblk -dn -o NAME', shell=True,
+                                    universal_newlines=True).splitlines()
+    except subprocess.CalledProcessError:
+        print("CRITICAL: could not get list of physical drives")
+        exit(2)
 
-# Run through block devices found
-for blk_device in blkdev_list:
-    if blk_device.startswith('sd'):
-        drive = BlkDev(blk_device.split()[0])
-        if drive.vendor == 'MegaRaid':
-            check4tool('storcli64')
-            try:
-                megaraid_raid_controllers
-            except NameError:
-                megaraid_raid_controllers = RaidControllersMegaRaid()
-        elif drive.vendor == 'Adaptec':
-            check4tool('arcconf')
-            try:
-                adaptec_raid_controllers
-            except NameError:
-                adaptec_raid_controllers = RaidControllersAdaptec()
+    # Run through block devices found
+    for blk_device in blkdev_list:
+        if blk_device.startswith('sd'):
+            drive = BlkDev(blk_device.split()[0])
+            if drive.vendor == 'MegaRaid':
+                check4tool('storcli64')
+                try:
+                    megaraid_raid_controllers
+                except NameError:
+                    megaraid_raid_controllers = RaidControllersMegaRaid()
+
+                raid_array = megaraid_raid_controllers.fetch_array(
+                    'blk_dev_name', blk_device)
+            elif drive.vendor == 'Adaptec':
+                check4tool('arcconf')
+                try:
+                    adaptec_raid_controllers
+                except NameError:
+                    adaptec_raid_controllers = RaidControllersAdaptec()
+
+                raid_array = adaptec_raid_controllers.fetch_array(
+                    'blk_dev_name', blk_device)
